33b5b26587df4083abe82aee2f8af85606e5b9b7
Peter Kirschner <peter@kirschners.de>
2019-07-01 10:13:51 +0200

    Enable usage of System properties instead of args

--- io.klib.tools/src/io/klib/tools/signing/Launcher.java
+++ io.klib.tools/src/io/klib/tools/signing/Launcher.java
@@ -19,14 +19,32 @@ public class Launcher {
 		if (args[0].equalsIgnoreCase(USAGE_UNSIGN)) {
 			JarUnsigner.unsignJarFiles(args[1]);
 		} else if (args[0].equalsIgnoreCase(USAGE_SIGN)) {
-			Properties props = new Properties();
-			try {
-				props.load(new File(args[1]).toURI().toURL().openStream());
-				JarSigner.signJarFiles(props, args[2]);
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+			if (args.length == 3) {
+				Properties props = new Properties();
+				try {
+					props.load(new File(args[1]).toURI().toURL().openStream());
+					JarSigner.signJarFiles(props, args[2]);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				Properties props = new Properties();
+				props.put("KEYSTORE", System.getProperty("KEYSTORE"));
+				props.put("STOREPASS", System.getProperty("STOREPASS"));
+				props.put("KEYPASS", System.getProperty("KEYPASS"));
+				props.put("ALIAS", System.getProperty("ALIAS"));
+				if (System.getProperty("TSA") != null) {
+					props.put("TSA", System.getProperty("TSA"));
+				}
+				if (System.getProperty("SIGFILE") != null) {
+					props.put("SIGFILE", System.getProperty("SIGFILE"));
+				}
+				if (System.getProperty("VERBOSE") != null) {
+					props.put("VERBOSE", System.getProperty("VERBOSE", "true"));
+				}
+				JarSigner.signJarFiles(props, args[1]);
 			}
 		} else if (args[0].equalsIgnoreCase(USAGE_VERIFY)) {
 			JarSigner.verifyJarFiles(args[1]);
