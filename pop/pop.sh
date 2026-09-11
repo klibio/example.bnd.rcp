bacb613398afb7c6a092ab1104e7e944401309b0
peterkir <peter@klib.io>
2026-09-11 16:56:12 +0200

    Publish branch-tagged Docker images

--- pop/pop.sh
+++ pop/pop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-scriptDir="$( cd $( dirname $0 ) >/dev/null 2>&1 && pwd )"
+set -u
+
+scriptDir="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 stdout=$scriptDir/pop.stdout
 result=$scriptDir/target/result.txt
 
@@ -10,25 +12,29 @@ else
     printf "# executing PoP inside $scriptDir\n"
 fi
 
-function executeProcess {
-  printf "## execute process $1\n"
-  $1 & PID1=$!
+executeProcess() {
+    local jarPath="$1"
+    printf "## execute process %s\n" "$jarPath"
+    /data/run-app.sh "$jarPath" &
+    local pid=$!
 
-  if [ -z "$PID1" ]
+    if kill -0 "$pid" 2>/dev/null
   then
-      echo "false" > $stdout
-      exit 1
+            printf "true\n" > "$stdout"
+            printf "## successfully launched %s\n" "$jarPath"
+            kill "$pid" 2>/dev/null || true
+            wait "$pid" 2>/dev/null || true
   else
-      echo "true" > $stdout
-      printf "## successfully launched $1\n"
-      kill $PID1
+            printf "false\n" > "$stdout"
+            printf "## failed to launch %s\n" "$jarPath"
+            exit 1
   fi
 }
 
-executeProcess "/data/jre/bin/java -jar /data/app.ui_linux.gtk.x86-64.jar"
-executeProcess "/data/jre/bin/java -jar /data/ui_linux.gtk.x86-64.jar"
-executeProcess "/data/jre/bin/java -jar /data/12_equinoxapp_linux.gtk.x86-64.jar"
+executeProcess /data/app.ui_linux.gtk.x86-64.jar
+executeProcess /data/ui_linux.gtk.x86-64.jar
+executeProcess /data/12_equinoxapp_linux.gtk.x86-64.jar
 
 echo "## copying execution result $stdout to mounted filesystem"
-cp $stdout $result
+cp "$stdout" "$result"
 echo "# done"
